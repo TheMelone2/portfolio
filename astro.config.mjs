d53bc159be75d92ed6d8450571d9f4d84947fd9a
Jan Luca <90137253+TheMelone2@users.noreply.github.com>
2024-06-22 23:53:03 +0200

    refactor: Update Astro configuration and workflow

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,12 @@
 import { defineConfig } from 'astro/config';
+import node from '@astrojs/node';
 
 // https://astro.build/config
 export default defineConfig({
     site: 'https://themelone2.github.io',
     base: 'portfolio',
-});
+    output: 'server',
+    adapter: node({
+      mode: 'standalone',
+    }),
+})
