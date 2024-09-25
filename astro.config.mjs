d929b9cb763ff520655db4907ac854c86b8abab4
Jan Luca <90137253+TheMelone2@users.noreply.github.com>
2024-09-25 18:31:12 +0200

    Update site URL in astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,5 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://themelone2.github.io'
+    site: 'https://themelon.live'
 })
